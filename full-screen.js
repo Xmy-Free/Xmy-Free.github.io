373edf16c57cde399689fe2c462f5a1d29434815
Xmy-Free <1459576494@qq.com>
2025-06-15 03:50:38 +0800

    Add files via upload

---
+++ full-screen.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', function () {
+    var elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}, { once: true }); // 使用 { once: true } 确保只在第一次点击时触发
